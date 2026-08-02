eaf5ca1559f0e1cf0fc7166963ac6f09c3833f43
WalGo235 <abdoulayeoumara20@gmail.com>
2026-08-02 18:03:47 +0100

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,12 @@
+import type { MetadataRoute } from "next";
 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/admin/",
+    },
+    sitemap: "https://tchadsportlive.com/sitemap.xml",
+  };
+}
