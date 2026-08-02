4ece5919356c6037427423cba5705b47a491d39d
WalGo235 <abdoulayeoumara20@gmail.com>
2026-08-02 12:33:08 +0100

    Update MatchGridSection.tsx

--- components/MatchGridSection.tsx
+++ components/MatchGridSection.tsx
@@ -1,1 +1,17 @@
+"use client";
 
+import MatchCard from "./MatchCard";
+import { useLiveMatches } from "@/lib/useLiveMatches";
+import type { MatchCardData } from "./MatchCard";
+
+export default function MatchGridSection({ initialMatches }: { initialMatches: MatchCardData[] }) {
+  const matches = useLiveMatches(initialMatches);
+
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      {matches.map((match) => (
+        <MatchCard key={match.id} match={match} />
+      ))}
+    </div>
+  );
+}
