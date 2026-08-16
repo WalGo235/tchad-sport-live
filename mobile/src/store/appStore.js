a4701fa24140fa05feebd93da35ddbd625c34a45
WalGo235 <abdoulayeoumara20@gmail.com>
2026-08-16 22:38:49 +0100

    Update appStore.js

--- mobile/src/store/appStore.js
+++ mobile/src/store/appStore.js
@@ -1,1 +1,22 @@
+import { create } from 'zustand';
 
+export const useAppStore = create((set) => ({
+  user: null,
+  favorites: [],
+  matches: [],
+  teams: [],
+  news: [],
+  standings: [],
+  
+  setUser: (user) => set({ user }),
+  addFavorite: (teamId) => set((state) => ({
+    favorites: [...new Set([...state.favorites, teamId])],
+  })),
+  removeFavorite: (teamId) => set((state) => ({
+    favorites: state.favorites.filter(id => id !== teamId),
+  })),
+  setMatches: (matches) => set({ matches }),
+  setTeams: (teams) => set({ teams }),
+  setNews: (news) => set({ news }),
+  setStandings: (standings) => set({ standings }),
+}));
